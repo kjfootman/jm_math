dbc0598286b266413a422692d35d91e2e8ee3c84
kjm <kjfootman@daum.net>
2026-07-05 23:46:39 +0900

    벡터 합 simd 구현

--- src/linear_algebra/simd.rs
+++ src/linear_algebra/simd.rs
@@ -8,10 +8,42 @@ pub fn arch() -> &'static Arch {
     ARCH.get_or_init(Arch::new)
 }
 
-/// Returns the optimum chunk size.
-/// The minimum chunk size is 1024.
+/// Calculates an optimized chunk size for parallel iterator processing
+/// using a combination of workload oversampling and SIMD lane alignment.
+///
+/// This utility ensures efficient dynamic load balancing via Rayon and
+/// minimizes scalar tail loop overhead during vectorized operations.
+///
+/// # Arguments
+///
+/// * `len` - The total number of elements in the collection or slice.
+///
+/// # Returns
+///
+/// An optimized chunk size that is at least 1024 and guaranteed to be a multiple of 8.
 pub fn calculate_chunk_size(len: usize) -> usize {
     let n_thread = rayon::current_num_threads();
 
     ((len / (n_thread * 4)).max(1024) + 7) & !7
+}
+
+pub struct VectorAdd<'a>(pub &'a mut [f64], pub &'a [f64]);
+impl<'a> WithSimd for VectorAdd<'a> {
+    type Output = ();
+
+    #[inline(always)]
+    fn with_simd<S: Simd>(self, simd: S) -> Self::Output {
+        let (head1, tail1) = S::as_mut_simd_f64s(self.0);
+        let (head2, tail2) = S::as_simd_f64s(self.1);
+
+        head1
+            .iter_mut()
+            .zip(head2.iter())
+            .for_each(|(v1, v2)| *v1 = simd.add_f64s(*v1, *v2));
+
+        tail1
+            .iter_mut()
+            .zip(tail2.iter())
+            .for_each(|(v1, v2)| *v1 += v2);
+    }
 }
--- src/linear_algebra/vector.rs
+++ src/linear_algebra/vector.rs
@@ -1,4 +1,6 @@
 use super::simd;
+use log::error;
+use rayon::prelude::*;
 use std::ops::{Add, Deref, DerefMut, Index, IndexMut, Range};
 
 #[derive(Debug)]
@@ -11,6 +13,28 @@ impl Vector {
         Self {
             values: vec![0.0; size],
         }
+    }
+
+    /// Performs in-place vector addition (`self += rhs`) using Rayon for multi-threading
+    /// and Pulp for runtime-dispatched SIMD vectorization.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the lengths of `self` and `rhs` are not equal.
+    fn add_assign(&mut self, rhs: &[f64]) {
+        let len = self.len();
+
+        if len != rhs.len() {
+            error!("Cannot add vectors of different dimensions");
+            panic!();
+        }
+
+        let arch = simd::arch();
+        let chunk_size = simd::calculate_chunk_size(len);
+
+        self.par_chunks_mut(chunk_size)
+            .zip(rhs.par_chunks(chunk_size))
+            .for_each(|(lhs, rhs)| arch.dispatch(simd::VectorAdd(lhs, rhs)));
     }
 }
 
@@ -62,13 +86,41 @@ impl From<Vec<f64>> for Vector {
     }
 }
 
-impl Add for Vector {
-    type Output = Vector;
+// impl Add for Vector {
+//     type Output = Vector;
 
-    fn add(self, rhs: Self) -> Self::Output {
-        Vector::new(1)
-    }
+//     /// `Vector` addition
+//     fn add(self, rhs: Self) -> Self::Output {
+//         assert_eq!(
+//             self.len(),
+//             rhs.len(),
+//             "Cannot add vectors of different dimensions"
+//         );
+
+//         let v: Vec<f64> = self
+//             .par_iter()
+//             .zip(rhs.par_iter())
+//             .map(|(v1, v2)| v1 + v2)
+//             .collect();
+
+//         Vector::from(v)
+//     }
+// }
+
+macro_rules! impl_vector_add {
+    (Vector, Vector) => {
+        impl Add<Vector> for Vector {
+            type Output = Vector;
+
+            fn add(mut self, rhs: Vector) -> Self::Output {
+                self.add_assign(&rhs);
+                self
+            }
+        }
+    };
 }
+
+impl_vector_add!(Vector, Vector);
 
 #[cfg(test)]
 mod tests {
@@ -76,7 +128,11 @@ mod tests {
 
     #[test]
     fn vector_test() {
-        let chunk_size = simd::calculate_chunk_size(100);
-        println!("{chunk_size}");
+        let v1 = Vector::from(vec![1.0, 2.0, 3.0]);
+        let v2 = Vector::from(vec![3.0, 2.0, 1.0]);
+
+        let v = v1 + v2;
+
+        println!("{v:#?}");
     }
 }
