415fc4e5a2ee357bce4a4530bda5737b8deef132
kjm <kjfootman@daum.net>
2026-07-10 00:14:20 +0900

    벡터 차/상수 곱/내적 구현 완료

--- src/linear_algebra/simd.rs
+++ src/linear_algebra/simd.rs
@@ -33,17 +33,83 @@ impl<'a> WithSimd for VectorAdd<'a> {
 
     #[inline(always)]
     fn with_simd<S: Simd>(self, simd: S) -> Self::Output {
-        let (head1, tail1) = S::as_mut_simd_f64s(self.0);
-        let (head2, tail2) = S::as_simd_f64s(self.1);
+        let (head0, tail0) = S::as_mut_simd_f64s(self.0);
+        let (head1, tail1) = S::as_simd_f64s(self.1);
 
-        head1
+        head0
             .iter_mut()
-            .zip(head2.iter())
+            .zip(head1.iter())
             .for_each(|(v1, v2)| *v1 = simd.add_f64s(*v1, *v2));
 
-        tail1
+        tail0
             .iter_mut()
-            .zip(tail2.iter())
+            .zip(tail1.iter())
             .for_each(|(v1, v2)| *v1 += v2);
+    }
+}
+
+pub struct VectorSub<'a>(pub &'a mut [f64], pub &'a [f64]);
+impl<'a> WithSimd for VectorSub<'a> {
+    type Output = ();
+
+    #[inline(always)]
+    fn with_simd<S: Simd>(self, simd: S) -> Self::Output {
+        let (head0, tail0) = S::as_mut_simd_f64s(self.0);
+        let (head1, tail1) = S::as_simd_f64s(self.1);
+
+        head0
+            .iter_mut()
+            .zip(head1.iter())
+            .for_each(|(v1, v2)| *v1 = simd.sub_f64s(*v1, *v2));
+
+        tail0
+            .iter_mut()
+            .zip(tail1.iter())
+            .for_each(|(v1, v2)| *v1 -= v2);
+    }
+}
+
+pub struct VectorMul<'a>(pub &'a mut [f64], pub f64);
+impl<'a> WithSimd for VectorMul<'a> {
+    type Output = ();
+
+    #[inline(always)]
+    fn with_simd<S: Simd>(self, simd: S) -> Self::Output {
+        let (head, tail) = S::as_mut_simd_f64s(self.0);
+        let scalar = self.1;
+        let scalar_simd = simd.splat_f64s(scalar);
+
+        head.iter_mut()
+            .for_each(|v| *v = simd.mul_f64s(scalar_simd, *v));
+
+        tail.iter_mut().for_each(|v| *v *= scalar);
+    }
+}
+
+pub struct VectorDot<'a>(pub &'a [f64], pub &'a [f64]);
+impl<'a> WithSimd for VectorDot<'a> {
+    type Output = f64;
+
+    #[inline(always)]
+    fn with_simd<S: Simd>(self, simd: S) -> Self::Output {
+        let (head0, tail0) = S::as_simd_f64s(self.0);
+        let (head1, tail1) = S::as_simd_f64s(self.1);
+        let mut c = simd.splat_f64s(0.0);
+
+        // a * b 결과를 acc 에 누적 : c = a * b + c
+        head0
+            .iter()
+            .zip(head1.iter())
+            .for_each(|(a, b)| c = simd.mul_add_f64s(*a, *b, c));
+
+        let mut result = simd.reduce_sum_f64s(c);
+
+        result += tail0
+            .iter()
+            .zip(tail1.iter())
+            .map(|(a, b)| a * b)
+            .sum::<f64>();
+
+        result
     }
 }
--- src/linear_algebra/vector.rs
+++ src/linear_algebra/vector.rs
@@ -1,7 +1,7 @@
 use super::simd;
 use log::error;
 use rayon::prelude::*;
-use std::ops::{Add, Deref, DerefMut, Index, IndexMut, Range};
+use std::ops::{Add, Deref, DerefMut, Index, IndexMut, Mul, Range, Sub};
 
 #[derive(Debug)]
 pub struct Vector {
@@ -15,12 +15,6 @@ impl Vector {
         }
     }
 
-    /// Performs in-place vector addition (`self += rhs`) using Rayon for multi-threading
-    /// and Pulp for runtime-dispatched SIMD vectorization.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the lengths of `self` and `rhs` are not equal.
     fn add_assign(&mut self, rhs: &[f64]) {
         let len = self.len();
 
@@ -35,6 +29,48 @@ impl Vector {
         self.par_chunks_mut(chunk_size)
             .zip(rhs.par_chunks(chunk_size))
             .for_each(|(lhs, rhs)| arch.dispatch(simd::VectorAdd(lhs, rhs)));
+    }
+
+    fn sub_assign(&mut self, rhs: &[f64]) {
+        let len = self.len();
+
+        if len != rhs.len() {
+            error!("Cannot subtract vectors of different dimensions");
+            panic!();
+        }
+
+        let arch = simd::arch();
+        let chunk_size = simd::calculate_chunk_size(len);
+
+        self.par_chunks_mut(chunk_size)
+            .zip(rhs.par_chunks(chunk_size))
+            .for_each(|(lhs, rhs)| arch.dispatch(simd::VectorSub(lhs, rhs)));
+    }
+
+    fn mul_assign(&mut self, rhs: f64) {
+        let len = self.len();
+        let arch = simd::arch();
+        let chunk_size = simd::calculate_chunk_size(len);
+
+        self.par_chunks_mut(chunk_size)
+            .for_each(|v| arch.dispatch(simd::VectorMul(v, rhs)));
+    }
+
+    fn dot(&self, rhs: &[f64]) -> f64 {
+        let len = self.len();
+
+        if len != rhs.len() {
+            error!("Cannot multiply vectors of different dimensions");
+            panic!();
+        }
+
+        let arch = simd::arch();
+        let chunk_size = simd::calculate_chunk_size(len);
+
+        self.par_chunks(chunk_size)
+            .zip(rhs.par_chunks(chunk_size))
+            .map(|(lhs, rhs)| arch.dispatch(simd::VectorDot(lhs, rhs)))
+            .sum::<f64>()
     }
 }
 
@@ -86,27 +122,6 @@ impl From<Vec<f64>> for Vector {
     }
 }
 
-// impl Add for Vector {
-//     type Output = Vector;
-
-//     /// `Vector` addition
-//     fn add(self, rhs: Self) -> Self::Output {
-//         assert_eq!(
-//             self.len(),
-//             rhs.len(),
-//             "Cannot add vectors of different dimensions"
-//         );
-
-//         let v: Vec<f64> = self
-//             .par_iter()
-//             .zip(rhs.par_iter())
-//             .map(|(v1, v2)| v1 + v2)
-//             .collect();
-
-//         Vector::from(v)
-//     }
-// }
-
 macro_rules! impl_vector_add {
     (Vector, Vector) => {
         impl Add<Vector> for Vector {
@@ -117,10 +132,117 @@ macro_rules! impl_vector_add {
                 self
             }
         }
+
+        impl Add<&Vector> for Vector {
+            type Output = Vector;
+
+            fn add(mut self, rhs: &Vector) -> Self::Output {
+                self.add_assign(rhs);
+                self
+            }
+        }
+
+        impl Add<Vector> for &Vector {
+            type Output = Vector;
+
+            fn add(self, mut rhs: Vector) -> Self::Output {
+                rhs.add_assign(self);
+                rhs
+            }
+        }
     };
 }
-
 impl_vector_add!(Vector, Vector);
+
+macro_rules! impl_vector_sub {
+    (Vector, Vector) => {
+        impl Sub<Vector> for Vector {
+            type Output = Vector;
+
+            fn sub(mut self, rhs: Vector) -> Self::Output {
+                self.sub_assign(&rhs);
+                self
+            }
+        }
+
+        impl Sub<&Vector> for Vector {
+            type Output = Vector;
+
+            fn sub(mut self, rhs: &Vector) -> Self::Output {
+                self.sub_assign(rhs);
+                self
+            }
+        }
+
+        impl Sub<Vector> for &Vector {
+            type Output = Vector;
+
+            fn sub(self, mut rhs: Vector) -> Self::Output {
+                rhs.sub_assign(self);
+                rhs
+            }
+        }
+    };
+}
+impl_vector_sub!(Vector, Vector);
+
+macro_rules! impl_vector_mul {
+    (f64, Vector) => {
+        impl Mul<Vector> for f64 {
+            type Output = Vector;
+
+            fn mul(self, mut rhs: Vector) -> Self::Output {
+                rhs.mul_assign(self);
+                rhs
+            }
+        }
+
+        impl Mul<f64> for Vector {
+            type Output = Vector;
+
+            fn mul(mut self, rhs: f64) -> Self::Output {
+                self.mul_assign(rhs);
+                self
+            }
+        }
+    };
+
+    (Vector, Vector) => {
+        impl Mul<Vector> for Vector {
+            type Output = f64;
+
+            fn mul(self, rhs: Vector) -> Self::Output {
+                self.dot(&rhs)
+            }
+        }
+
+        impl Mul<Vector> for &Vector {
+            type Output = f64;
+
+            fn mul(self, rhs: Vector) -> Self::Output {
+                rhs.dot(self)
+            }
+        }
+
+        impl Mul<&Vector> for Vector {
+            type Output = f64;
+
+            fn mul(self, rhs: &Vector) -> Self::Output {
+                self.dot(rhs)
+            }
+        }
+
+        impl Mul<&Vector> for &Vector {
+            type Output = f64;
+
+            fn mul(self, rhs: &Vector) -> Self::Output {
+                self.dot(rhs)
+            }
+        }
+    };
+}
+impl_vector_mul!(f64, Vector);
+impl_vector_mul!(Vector, Vector);
 
 #[cfg(test)]
 mod tests {
@@ -131,8 +253,17 @@ mod tests {
         let v1 = Vector::from(vec![1.0, 2.0, 3.0]);
         let v2 = Vector::from(vec![3.0, 2.0, 1.0]);
 
-        let v = v1 + v2;
-
+        // let v = v1 - &v2;
+        // let v = v1 - v2;
+        // let v = 3.0 * v1;
+        // let v = v1 * 3.0;
+        let v = &v1 * &v2;
         println!("{v:#?}");
     }
+
+    // fn test(mut a: f64) -> f64 {
+    //     a += 3.0;
+
+    //     a
+    // }
 }
