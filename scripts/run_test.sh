c852ce1c7a976ce3bd6b8609abb90a928bcde245
J. Kim <kjfootman@daum.net>
2026-08-24 06:43:55 +0000

    .DS_Store 파일추적 제거

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# TEST_FUNC="vector_from_mtx_test"
-# TEST_FUNC="gauss_seidel_test"
-TEST_FUNC="csr_diagonal_test"
+# TEST_NAME="vector_from_mtx_test"
+# TEST_NAME="csr_diagonal_test"
+TEST_NAME="gauss_seidel_test"
 
 BASE=$(dirname "$0")
 cd "$BASE/.." || exit && pwd
 
-RUST_LOG=jm_lib=debug cargo test -r "$TEST_FUNC" -- --nocapture
+RUST_LOG=jm_lib=debug cargo test -r "$TEST_NAME" -- --nocapture
